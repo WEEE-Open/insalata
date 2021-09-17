efd4e4862b4c68d141fbcacac9430d304fe54dfe
e-caste-mbp <git@caste.dev>
2021-09-17 19:31:48 +0200

    more helpful messages

--- cucchiaio.sh
+++ cucchiaio.sh
@@ -56,7 +56,7 @@ make up
 make examples
 cd ..
 xdg-open "$TARALLO_URL"
-echo -e "\nT.A.R.A.L.L.O. was successfully installed!\nIt is available at $TARALLO_URL\nYou can shut it down from this directory with: docker-compose down\n"
+echo -e "\nT.A.R.A.L.L.O. was successfully installed!\nIt is available at $TARALLO_URL\nYou can shut it down from $PWD/tarallo with: make down\n"
 
 echo -e "\nInstalling WEEEhire-ng...\n"
 rm_x_dir weeehire-ng
@@ -67,7 +67,7 @@ cp config/config-example.php config/config.php
 docker-compose up -d
 cd ..
 xdg-open "$WEEEHIRE_URL"
-echo -e "\nWEEEhire-ng was successfully installed!\nIt is available at $WEEEHIRE_URL\nYou can shut it down from this directory with: docker-compose down\n"
+echo -e "\nWEEEhire-ng was successfully installed!\nIt is available at $WEEEHIRE_URL\nYou can shut it down from $PWD/weeehire-ng with: make down\n"
 
 echo -e "\nInstalling P.E.R.A.C.O.T.T.A. (Progetto Esteso Raccolta Automatica Configurazioni hardware Organizzate Tramite Tarallo Autonomamente)...\n"
 rm_x_dir peracotta
@@ -79,7 +79,7 @@ pip install -r requirements.txt
 python main.py --gui
 deactivate
 cd ..
-echo -e "\nP.E.R.A.C.O.T.T.A. was successfully installed!\n"
+echo -e "\nP.E.R.A.C.O.T.T.A. was successfully installed!\nYou can run it from $PWD/peracotta with: python main.py --gui\n"
 
 echo -e "\nInstalling P.E.S.T.O. (Progetto di Erase Smart con Taralli Olistici)...\n"
 rm_x_dir pesto
@@ -92,7 +92,7 @@ pip install -r requirements_server.txt
 python pinolo.py
 deactivate
 cd ..
-echo -e "\nP.E.S.T.O. successfully installed!\n"
+echo -e "\nP.E.S.T.O. successfully installed!\nYou can run it from $PWD/pesto with: python pinolo.py\n"
 
 echo -e "\nInstalling S.A.R.D.I.N.A. (Statistiche Amabili Rendimento Degli Informatici Nell’Anno)...\n"
 rm_x_dir sardina
@@ -104,5 +104,5 @@ sed -i 's/keep_repos = False/keep_repos = True/g' config.py
 # run S.A.R.D.I.N.A. in a new terminal window
 xterm python main.py --cloc --commits --sloc --graphs --lang
 cd ..
-echo -e "\nS.A.R.D.I.N.A. was successfully installed!\nYou can also run it with: docker run --rm -v $PWD/output:/sardina/output\n"
+echo -e "\nS.A.R.D.I.N.A. was successfully installed!\nYou can run it from $PWD/sardina with: python main.py --cloc --commits --sloc --graphs --lang\nYou can also run it with: docker run --rm -v \$PWD/output:/sardina/output\n"
 
